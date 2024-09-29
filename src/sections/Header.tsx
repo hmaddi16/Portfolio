5fddb0a52cfbe7c6bcb6ba648b1d1bf698c7ebaa
HarshithaMaddi <hmaddi61@gmail.com>
2024-09-29 19:57:24 -0400

    disable home btn

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -13,16 +13,19 @@ export const Header = () => {
   };
 
   return (
-    <div className="flex justify-center items-center fixed top-5 w-full z-10">
+    <div className="flex justify-center items-center fixed top-6 w-full z-10 sm:top-8">
 
       {/* Navigation Bar */}
       <nav className="flex gap-1 p-1 border border-white/15 rounded-full bg-color-secondary backdrop-blur">
+        {/* 
+        HOME BUTTON DISABLED
         <a href="#hero"
           className={`nav-item ${activeLink === "#hero" ? "active" : ""}`}
           onClick={() => handleClick("#hero")}
         >
           Home
         </a>
+        */}
 
         <a
           href="#about"
@@ -59,3 +62,5 @@ export const Header = () => {
     </div>
   );
 };
+
+
